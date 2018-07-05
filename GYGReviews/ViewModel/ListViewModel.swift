441707acfe691a9baa43cbddc8aa0b10d51e5c75
Anuraag Shakya <anuraag.shakya@gmail.com>
2018-07-05 13:26:59 +0200

    Add  as a Cocoapod

---
+++ GYGReviews/ViewModel/ListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  ListViewModel.swift
+//  GYGReviews
+//
+//  Created by Anuraag Shakya on 05.07.18.
+//  Copyright © 2018 Bhunte. All rights reserved.
+//
+
+import Foundation
+
+struct ListViewModel {
+    
+}
