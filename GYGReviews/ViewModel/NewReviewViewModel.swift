43306f8ec6a96c399cb0de17445c37bda18e87c3
Anuraag Shakya <anuraag.shakya@gmail.com>
2018-07-05 23:56:48 +0200

    Add mock API to POST a new review to the server

--- GYGReviews/ViewModel/NewReviewViewModel.swift
+++ GYGReviews/ViewModel/NewReviewViewModel.swift
@@ -10,24 +10,53 @@ import Foundation
 
 class NewReviewViewModel {
     func submitNewReviewWith(title: String, message: String, rating: String) {
-        // Do submission here
-        let formatter = DateFormatter()
-        formatter.dateFormat = "MMMM dd, yyyy"
-        let date = formatter.string(from: Date())
-        let dateUnformatted = ""
-        let language = UserDefaults.standard.string(forKey: "language")!
-        let foreignLanguage = language != "en"
-        let reviewerCountry = UserDefaults.standard.string(forKey: "country")!
-        let reviewerName = UserDefaults.standard.string(forKey: "userName")!
-        let author = reviewerName + " - " + reviewerCountry
+        // This function sets up the HTTP POST request and transmits it
+        
+        // Get user ID
         let reviewer_id = UserDefaults.standard.integer(forKey: "userID")
-        let traveler_type = UserDefaults.standard.string(forKey: "travelerType")!
-
-        let review = Review(
-            author: author, date: date, dateUnformatted: dateUnformatted, foreignLanguage: foreignLanguage, languageCode: language, message: message, rating: rating, reviewerCountry: reviewerCountry, reviewerName: reviewerName, review_id: reviewer_id, title: title, traveler_type: traveler_type)
         
-        // use the data to send POST request
+        // Get URLSession shared object
+        let session = URLSession.shared
         
-        print(review)
+        // Create URL request from url string
+        let url = URL(string: "https://www.getyourguide.com/berlin-l17/tempelhof-2-hour-airport-history-tour-berlin-airlift-more-t23776/review")!
+        var request = URLRequest(url: url)
+        
+        // Set POST parameters
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        let postString = "id=\(reviewer_id)&title=\(title)&message=\(message)&rating=\(rating)"
+        request.httpBody = postString.data(using: .utf8)
+        
+        // Create the data task for the shared session to carry out
+        let task = session.dataTask(with: request) { data, response, error in
+            // Guard for network errors
+            guard error == nil else {
+                print("Network error: \(error!.localizedDescription)")
+                // TODO: Notify user of error
+                return
+            }
+            
+            // Guard for absence of data
+            guard let data = data else {
+                print("Network error: No data received")
+                // TODO: Notify user of error
+                return
+            }
+            
+            // Check if response status is 200 (OK), else return
+            if let httpStatus = response as? HTTPURLResponse, httpStatus.statusCode != 200 {
+                // check for http errors
+                print("HTTP error: statusCode should be 200, but is \(httpStatus.statusCode)")
+                // TODO: Notify user of error
+                return
+            }
+            
+            let responseString = String(data: data, encoding: .utf8) ?? ""
+            print("responseString = \(responseString)")
+        }
+        
+        // Do the task to send data to server
+        task.resume()
     }
 }
